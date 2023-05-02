a2910afe506815a7a1215e048f0f6d13f1c5e40d
Camila Lopes <camilacslopes@gmail.com>
2023-05-02 08:48:09 -0300

    feat: implement wordFrequency and topWords function

--- test/netlex-dev-test/backend/app/helpers/document.helper.js
+++ test/netlex-dev-test/backend/app/helpers/document.helper.js
@@ -1,18 +1,30 @@
 module.exports = {
-    wordFrequency: function(doc, word){
-        throw new Error("Not implemented exception")
+    stringToWordsArray: function(text) {
+      return text.match(/[a-zÀ-ú]+/gmui);
+    },
+  
+    wordFrequency: function(doc, word) {
+      doc = doc.toLowerCase();
+      word = word.toLowerCase();
+  
+      const words = this.stringToWordsArray(doc);
+  
+      let count = 0;
+      for (let i = 0; i < words.length; i++) {
+        if (words[i] === word) {
+          count++;
+        }
+      }
+  
+      return count;
     },
 
     wordSentences: function(doc, word){
-        throw new Error("Not implemented exception")
-    },
-
-    topWords: function(doc, count, minWordLength){
-        throw new Error("Not implemented exception")
-    },
-
-    stringToWordsArray: function(text) {
-        return text.match(/[a-zÀ-ú]+/gmui);
+      throw new Error("Not implemented exception")
+  },
+      
+    topWords: function(doc, count, minWordLength) {
+      throw new Error("Not implemented exception");
     }
-}
-
+  };
+  
